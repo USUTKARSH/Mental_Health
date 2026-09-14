0538938c5fc5ff8f817bfa1d6a84c4b03a67e282
USUTKARSH <usutkarsh43@gmail.com>
2026-09-14 21:25:44 +0530

    Map root health endpoint correctly

---
+++ backend/src/main/java/com/mentalhealth/controller/HealthController.java
@@ -0,0 +1,13 @@
+package com.mentalhealth.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+    @GetMapping("/")
+    public ResponseEntity<String> root() {
+        return ResponseEntity.ok("MindCare API is running");
+    }
+}
